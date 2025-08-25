9f31b22c48e1a54cf27ecedd291f267db8fb67d0
waris hayat <warishayat666@gmail.com>
2025-08-25 17:46:55 +0500

    Types and return Types

---
+++ app.js
@@ -0,0 +1,34 @@
+// itna haaala ku hae typeScript k baray m?
+// js ko 15 din m bnaya gya h unko sudharty sudharty aj ka dur agya hae...aur kch chzu m
+// thura short rehti hae un chzu ko typescript thk krti hae ,proper rule k sth
+// TYPESCRIPT dur karti uncertainity js k
+// Basic: Types=======> lets understand the basic types two types of types
+// Numbers,String,Boolean
+// Boolean,Array,
+//tuples , any ,unknown void,enums
+//1: Primitive
+//number,bolean,undefined,string,etc
+//2: Referance
+// object classsesss arrays tuples are called referances.
+var ab = "hello world";
+var b = false;
+var c = 1221;
+console.log(ab);
+console.log(b);
+console.log(c);
+// let get some other example of array and tuple
+var arr2 = [true, 12, "hello world"];
+arr2.push(4);
+console.log(arr2);
+// function return 
+function abcd() {
+    return "abcd";
+}
+abcd();
+// Function return void
+function cdef() {
+    console.log("Hello i am working");
+}
+cdef();
+// function which return never
+// will be same like this
