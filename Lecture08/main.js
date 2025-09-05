63717c13fa9436647fd474cd6d57f4eee20b32a6
waris hayat <warishayat666@gmail.com>
2025-09-05 15:58:58 +0500

    Types Assertion

---
+++ Lecture08/main.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var abc;
+abc = "waris";
+console.log(abc);
+var num;
+num = 12;
+// num = 200
+console.log(num);
+// let age: true;
+// age = false
+// console.log(age) //eror we cant gave the age to false it true always
+var dob;
+dob = true;
+console.log(dob);
+var glasses;
+// glasses = "hello"  this will creat error to keep bame armani or 202
+glasses = "Armani";
+console.log(glasses);
+glasses = 202;
+console.log(glasses);
---
+++ Lecture08/main.ts
@@ -0,0 +1,31 @@
+import { log } from "console";
+
+let abc:"waris";
+
+abc ="waris";
+
+console.log(abc);
+
+
+let num:12;
+num = 12;
+// num = 200
+console.log(num);
+
+
+// let age: true;
+// age = false
+// console.log(age) //eror we cant gave the age to false it true always
+
+
+let dob:true;
+dob=true;
+console.log(dob);
+
+
+let glasses : "Armani" | 202 ;
+// glasses = "hello"  this will creat error to keep bame armani or 202
+glasses = "Armani"
+console.log(glasses);
+glasses=202;
+console.log(glasses);
